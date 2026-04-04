cb3813dd46c8a0f06f070909162706f97fb76e46
BGE31-Thomas <tpjarles@gmail.com>
2026-04-04 10:39:17 +0200

    petite correction affichage

--- public/assets/js/openSourceDialog.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('openSourceModal').addEventListener('click', async () => {
-    const response = await fetch('/admin/source/add');
-    const html = await response.text();
-
-    document.getElementById('modalContainer').innerHTML = html;
-
-    document.getElementById('modal').showModal();
-});
